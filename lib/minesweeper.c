70f6451e4c0666a1aa9a075f1894e9fa15a6ee88
laazyLlama <laazy.llama@gmail.com>
2020-12-24 09:58:46 +0100

    first commit

---
+++ lib/minesweeper.c
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+//Project Name:     minesweeper/mineswee[er].c
+//Description:      The classic game "Minesweeper" built in c
+//-----------------------------------------------------------------------------
+//Author:           laazyLlama
+//Date Created:     24/12/2020
+//Last Changes:     24/12/2020
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+
+int main() {
+  return 0;
+}
